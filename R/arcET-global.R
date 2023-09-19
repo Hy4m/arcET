871c5dcf8db39a5d16b3b5201b03e52796e4a84f
houyun <houyunhuang@163.com>
2023-09-20 00:01:45 +0800

    global settings

---
+++ R/arcET-global.R
@@ -0,0 +1,97 @@
+## New environment that holds a set of global variables and settings for arcET
+arcET_global <- new.env(parent = emptyenv())
+
+## init index setting
+arcET_global$cell_index <- 0
+arcET_global$sector_index <- 0
+arcET_global$track_index <- 0
+
+## set global `steps` and `simplify`
+arcET_global$steps <- 0.005
+arcET_global$simplify <- TRUE
+
+#' @title Get ids
+#' @description Helper functions used to generate "CellID", "TrackID" and "SectorID".
+#' @param which if "all" will reset all id's index.
+#' @return `*_id()` return a character vector.
+#' @rdname IDs
+#' @author Hou Yun
+#' @export
+#' @examples
+#' cell_id()
+#' ## increment 1
+#' cell_id()
+#'
+#' ## reset
+#' reset_ids()
+#' cell_id()
+cell_id <- function() {
+  old <- get_arcET_global(which = "cell_index", default = 0)
+  arcET_global$cell_index <- old + 1
+  paste0("CellID_", old + 1)
+}
+
+#' @rdname IDs
+#' @export
+track_id <- function() {
+  old <- get_arcET_global(which = "track_index", default = 0)
+  arcET_global$track_index <- old + 1
+  paste0("TrackID_", old + 1)
+}
+
+#' @rdname IDs
+#' @export
+sector_id <- function() {
+  old <- get_arcET_global(which = "sector_index", default = 0)
+  arcET_global$sector_index <- old + 1
+  paste0("SectorID_", old + 1)
+}
+
+#' @rdname IDs
+#' @export
+reset_ids <- function(which = c("all", "CellID", "TrackID", "SectorID")) {
+  which <- match.arg(which)
+  if (which == "all") {
+    set_arcET_global(cell_index = 0,
+                     track_index = 0,
+                     sector_index = 0)
+  } else {
+    if (which == "CellID") {
+      set_arcET_global(cell_index = 0)
+    } else if (which == "TrackID") {
+      set_arcET_global(track_index = 0)
+    } else {
+      set_arcET_global(sector_index = 0)
+    }
+  }
+
+  invisible()
+}
+
+#' @title Get and set global options
+#' @description Helper functions used to get and set global options.
+#' @param ... any options can be defined, using name = value. Currently,
+#' only the `cell_index`,`sector_index`,`track_index`,`steps` and `simplify`
+#' options are used
+#' @param which a character string indicating which global setting will be returned.
+#' @param default if this global setting is not setting, the default will be returned.
+#' @return for `set_arcET_global()`, will return invisible `NULL`. And for `get_arcET_global()`,
+#' will return global setting or default.
+#' @rdname set_get_global
+#' @author Hou Yun
+#' @export
+set_arcET_global <- function(...) {
+  dots <- rlang::dots_list(..., .named = TRUE, .homonyms = "last")
+
+  for (nm in names(dots)) {
+    arcET_global[[nm]] <- dots[[nm]]
+  }
+
+  invisible()
+}
+
+#' @rdname set_get_global
+#' @export
+get_arcET_global <- function(which, default = NULL) {
+  arcET_global[[which]] %||% default
+}
