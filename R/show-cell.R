0219108e0254d6103ce37dab0bce4eb992b06146
houyun <houyunhuang@163.com>
2023-09-24 19:21:26 +0800

    improve show_cell()

--- R/show-cell.R
+++ R/show-cell.R
@@ -1,71 +1,97 @@
 #' Show CELL
 #' @description A quick way to show region of `CELL()`.
-#' @param region a CELL object (created by `CELL()` function) used to set
+#' @param ... any CELL objects (created by `CELL()` function) used to set
 #' the drawing area.
 #' @param fill fill colour of region.
 #' @param colour border colour of region.
+#' @param background,border fill and border colour of plot region (unit circle).
 #' @param digits integer indicating the number of decimal places.
 #' @param xn,yn number of xaxis/yaxis ticks.
 #' @param newpage logical, indicating whether a new graphics device needs to be opened.
 #' @param vp grid viewport object.
-#' @param ... other parameters passing to `ArcRectGrob()`.
 #' @return invisible grobs.
 #' @rdname show_cell
 #' @author Hou Yun
 #' @export
 #' @examples
-#' show_cell(CELL(start = 120, end = 60, r0 = 0.5))
-show_cell <- function(region = CELL(),
-                      fill = "grey90",
+#' show_cell(CELL(120, 60, 0.6))
+#' show_cell(CELL(120, 60, 0.6), CELL(300, 240, 0.3, 0.6))
+show_cell <- function(...,
+                      fill = "lightblue",
                       colour = NA,
+                      background = "grey95",
+                      border = NA,
                       digits = 2,
                       xn = 7,
                       yn = 7,
                       newpage = is.null(vp),
-                      vp = NULL,
-                      ...) {
-  stopifnot(is_CELL(region))
-
-  xseq <- seq(region$x.range[1], region$x.range[2], length.out = xn) %% 360
-  x_labels <- paste0(round(xseq, digits = digits), iconv("\u00b0", to = "UTF-8"))
-  yseq <- seq(region$y.range[1], region$y.range[2], length.out = yn)
-  y_labels <- as.character(round(yseq, digits = digits))
-  y_breaks <- if (region$r0 > region$r1) {
-    seq(1, 0, length.out = yn)
-  } else {
-    seq(0, 1, length.out = yn)
+                      vp = NULL) {
+  dots <- list(...)
+  if (length(dots) < 1) {
+    cli::cli_inform("No `region` input.")
+    invisible()
   }
-  coord <- PANEL(x_limits = c(0, 1),
-                 x_breaks = seq(0, 1, length.out = xn),
-                 x_labels = x_labels,
-                 x_range = c(0, 1),
-                 y_limits = c(0, 1),
-                 y_breaks = y_breaks,
-                 y_labels = y_labels,
-                 y_range = c(0, 1))
 
-  xaxis <- ArcxAxisGrob(coord = coord, region = region)
-  yaxis <- ArcyAxisGrob(coord = coord, region = region)
-  panel <- ArcPanelGrob(region = region, fill = fill, colour = colour, ...)
-  direct_x <- ArcSegmentsGrob(x = region$x.range[1],
-                              xend = region$x.range[2],
-                              y = min(region$y.range),
-                              yend = min(region$y.range),
-                              colour = "red",
-                              arrow = grid::arrow(length = unit(0.3, "cm")))
-  direct_y <- ArcSegmentsGrob(x = min(region$x.range),
-                              xend = min(region$x.range),
-                              y = region$y.range[1],
-                              yend = region$y.range[2],
-                              colour = "red",
-                              arrow = grid::arrow(length = unit(0.3, "cm")))
-  grobs <- gTree(children = gList(panel, xaxis, yaxis, direct_x, direct_y),
-                 vp = viewport(xscale = c(-1, 1), yscale = c(-1, 1)))
+  if (!all(vapply_lgl(dots, is_CELL))) {
+    cli::cli_abort("All `region` should be created by {.fn CELL}")
+  }
+
   gt <- gtable(widths = unit(c(1.5, 1, 1.5), c("cm", "null", "cm")),
                heights = unit(c(1.5, 1, 1.5), c("cm", "null", "cm")),
                respect = TRUE)
-  gt <- gtable_add_grob(gt, grobs, t = 2, l = 2, clip = "off",
-                        name = "Cell-Region")
+
+  bg <- grid::circleGrob(x = 0,
+                         y = 0,
+                         r = 1,
+                         default.units = "native",
+                         vp = viewport(xscale = c(-1, 1), yscale = c(-1, 1)),
+                         gp = gpar(col = border, fill = background))
+  gt <- gtable_add_grob(gt, bg, t = 2, l = 2, z = -Inf, clip = "off",
+                        name = "background")
+
+  for (ii in 1:length(dots)) {
+    REGION <- dots[[ii]]
+    xseq <- seq(REGION$x.range[1], REGION$x.range[2], length.out = xn) %% 360
+    x_labels <- paste0(round(xseq, digits = digits), iconv("\u00b0", to = "UTF-8"))
+    yseq <- seq(REGION$y.range[1], REGION$y.range[2], length.out = yn)
+    y_labels <- as.character(round(yseq, digits = digits))
+    y_breaks <- if (REGION$r0 > REGION$r1) {
+      seq(1, 0, length.out = yn)
+    } else {
+      seq(0, 1, length.out = yn)
+    }
+    coord <- PANEL(x_limits = c(0, 1),
+                   x_breaks = seq(0, 1, length.out = xn),
+                   x_labels = x_labels,
+                   x_range = c(0, 1),
+                   y_limits = c(0, 1),
+                   y_breaks = y_breaks,
+                   y_labels = y_labels,
+                   y_range = c(0, 1))
+
+
+    xaxis <- ArcxAxisGrob(coord = coord, region = REGION)
+    yaxis <- ArcyAxisGrob(coord = coord, region = REGION)
+    panel <- ArcPanelGrob(region = REGION, fill = fill, colour = colour)
+    direct_x <- ArcSegmentsGrob(x = REGION$x.range[1],
+                                xend = REGION$x.range[2],
+                                y = min(REGION$y.range),
+                                yend = min(REGION$y.range),
+                                colour = "red",
+                                arrow = grid::arrow(length = unit(0.3, "cm")))
+    direct_y <- ArcSegmentsGrob(x = min(REGION$x.range),
+                                xend = min(REGION$x.range),
+                                y = REGION$y.range[1],
+                                yend = REGION$y.range[2],
+                                colour = "red",
+                                arrow = grid::arrow(length = unit(0.3, "cm")))
+    grobs <- gTree(children = gList(panel, xaxis, yaxis, direct_x, direct_y),
+                   vp = viewport(xscale = c(-1, 1), yscale = c(-1, 1)),
+                   name = "Cell-Region")
+    gt <- gtable_add_grob(gt, grobs, t = 2, l = 2, clip = "off",
+                          name = paste("Cell-Region", ii, sep = "-"))
+  }
+
   if (isTRUE(newpage)) {
     grid::grid.newpage()
   }
