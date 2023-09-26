c1228a124cc31a8043b941566d0d0ca341980e84
houyun <houyunhuang@163.com>
2023-09-26 21:51:04 +0800

    add env parameter

--- R/arcET-global.R
+++ R/arcET-global.R
@@ -13,6 +13,7 @@ arcET_global$simplify <- TRUE
 #' @title Get ids
 #' @description Helper functions used to generate "CellID", "TrackID" and "SectorID".
 #' @param which if "all" will reset all id's index.
+#' @param env the environment in which IDs is to be generated.
 #' @return `*_id()` return a character vector.
 #' @rdname IDs
 #' @author Hou Yun
@@ -25,43 +26,48 @@ arcET_global$simplify <- TRUE
 #' ## reset
 #' reset_ids()
 #' cell_id()
-cell_id <- function() {
-  old <- get_arcET_global(which = "cell_index", default = 0)
-  arcET_global$cell_index <- old + 1
+cell_id <- function(env = NULL) {
+  env <- env %||% arcET_global
+  old <- get_arcET_global(which = "cell_index", default = 0, env = env)
+  env$cell_index <- old + 1
   paste0("CellID_", old + 1)
 }
 
 #' @rdname IDs
 #' @export
-track_id <- function() {
-  old <- get_arcET_global(which = "track_index", default = 0)
-  arcET_global$track_index <- old + 1
+track_id <- function(env = NULL) {
+  env <- env %||% arcET_global
+  old <- get_arcET_global(which = "track_index", default = 0, env = env)
+  env$track_index <- old + 1
   paste0("TrackID_", old + 1)
 }
 
 #' @rdname IDs
 #' @export
-sector_id <- function() {
-  old <- get_arcET_global(which = "sector_index", default = 0)
-  arcET_global$sector_index <- old + 1
+sector_id <- function(env = NULL) {
+  env <- env %||% arcET_global
+  old <- get_arcET_global(which = "sector_index", default = 0, env = env)
+  env$sector_index <- old + 1
   paste0("SectorID_", old + 1)
 }
 
 #' @rdname IDs
 #' @export
-reset_ids <- function(which = c("all", "CellID", "TrackID", "SectorID")) {
+reset_ids <- function(which = c("all", "CellID", "TrackID", "SectorID"), env = NULL) {
+  env <- env %||% arcET_global
   which <- match.arg(which)
   if (which == "all") {
     set_arcET_global(cell_index = 0,
                      track_index = 0,
-                     sector_index = 0)
+                     sector_index = 0,
+                     env = env)
   } else {
     if (which == "CellID") {
-      set_arcET_global(cell_index = 0)
+      set_arcET_global(cell_index = 0, env = env)
     } else if (which == "TrackID") {
-      set_arcET_global(track_index = 0)
+      set_arcET_global(track_index = 0, env = env)
     } else {
-      set_arcET_global(sector_index = 0)
+      set_arcET_global(sector_index = 0, env = env)
     }
   }
 
@@ -75,16 +81,19 @@ reset_ids <- function(which = c("all", "CellID", "TrackID", "SectorID")) {
 #' options are used
 #' @param which a character string indicating which global setting will be returned.
 #' @param default if this global setting is not setting, the default will be returned.
+#' @param env the environment in which IDs is to be generated. If is NULL (default), will
+#' evaluated in `arcET_global` environment.
 #' @return for `set_arcET_global()`, will return invisible `NULL`. And for `get_arcET_global()`,
 #' will return global setting or default.
 #' @rdname set_get_global
 #' @author Hou Yun
 #' @export
-set_arcET_global <- function(...) {
+set_arcET_global <- function(..., env = NULL) {
+  env <- env %||% arcET_global
   dots <- rlang::dots_list(..., .named = TRUE, .homonyms = "last")
 
   for (nm in names(dots)) {
-    arcET_global[[nm]] <- dots[[nm]]
+    env[[nm]] <- dots[[nm]]
   }
 
   invisible()
@@ -92,6 +101,7 @@ set_arcET_global <- function(...) {
 
 #' @rdname set_get_global
 #' @export
-get_arcET_global <- function(which, default = NULL) {
-  arcET_global[[which]] %||% default
+get_arcET_global <- function(which, default = NULL, env = NULL) {
+  env <- env %||% arcET_global
+  env[[which]] %||% default
 }
